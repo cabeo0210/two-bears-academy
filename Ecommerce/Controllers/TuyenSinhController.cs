658114b4f5429a6b06f4c91bbdeb24988dc573a1
WiBuMoney <cowsmile1911@gmail.com>
2023-11-02 23:29:00 +0700

    UI updated

---
+++ Ecommerce/Controllers/TuyenSinhController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers;
+
+public class TuyenSinhController : Controller
+{
+    private EcommerceDbContext _dbContext;
+    
+    public IActionResult Sale()
+    {
+
+
+        return Json("This is sale manager page");
+    }
+    
+    public IActionResult TuyenSinh()
+    {
+
+
+        return View();
+    }
+    
+    public IActionResult Lead()
+    {
+
+
+        return Json("This is lead manager page");
+    }
+}
---
+++ EcommerceCore/ViewModel/User/LeaderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceCore.Models;
+
+namespace EcommerceCore.ViewModel.User;
+
+public class LeaderViewModel : BaseUser
+{
+    public bool LeaderStatus { get; set; }
+    public string LeaderNotes { get; set; }
+    public string IsInvitedFrom { get; set; }
+    
+    public List<SalerViewModel> SalerViewModels { get; set; }
+}
